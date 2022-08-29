94c581a14bb9f6c9c95c2670f6c352517d8a058d
darjoshi <darjoshi@LIN20002842.corp.capgemini.com>
2022-08-29 15:36:30 +0530

    commit1

--- eclipse/SampleProject/src/com/cg/jsf/Demo.java
+++ eclipse/SampleProject/src/com/cg/jsf/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Fisrt git project");
 		System.out.println("2nd Commit");
+		System.out.println("Pulled from git");
 	}
 
 }
